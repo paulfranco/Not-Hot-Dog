36520045ae4436941b66dc525e65c58ad9ef7c52
Paul Franco <mail@paulfran.co>
2020-09-19 05:10:05 -0700

    Initial Commit

---
+++ Not Hot Dog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Not Hot Dog
+//
+//  Created by Paul Franco on 9/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
