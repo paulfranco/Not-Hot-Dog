4c4f38c44fb64138c5c5b95693207673611ef733
Paul Franco <mail@paulfran.co>
2020-09-19 18:02:20 -0700

    handle prediction results

--- Not Hot Dog/PreviewViewController.swift
+++ Not Hot Dog/PreviewViewController.swift
@@ -12,6 +12,7 @@ import Vision
 class PreviewViewController: UIViewController {
     
     @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var resultPhoto: UIImageView!
     var image: UIImage!
     
     override func viewDidLoad() {
@@ -32,11 +33,27 @@ class PreviewViewController: UIViewController {
         }
         let request = VNCoreMLRequest(model: model) { (VNRequest, error) in
             print(VNRequest.results?.first)
-            guard let results = VNRequest.results as? [VNClassificationObservation] else {
+            guard let results = VNRequest.results as? [VNClassificationObservation], let firstResult = results.first else {
                 fatalError("Unexpected results")
             }
             print(results.first?.confidence)
             print(results.first?.identifier)
+            
+            DispatchQueue.main.async {
+                let imageName = firstResult.identifier.contains("hotdog") ? "ishotdog" : "nothotdog"
+                
+                self.resultPhoto.image = UIImage(named: imageName)
+//                if firstResult.identifier.contains("hotdog") {
+//                    print("HOTDOG")
+//                    self.resultPhoto.image = UIImage(named: "ishotdog")
+//                } else {
+//                    print("NOT HOTDOG")
+//                    self.resultPhoto.image = UIImage(named: "nothotdog")
+//
+//                }
+                
+            }
+            
         }
         let handler = VNImageRequestHandler(ciImage: ciImage)
         
